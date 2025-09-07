bdd03ed790688fd0ed9189162df5bbb04a53d971
aeo711 <adreaojukwu@gmail.com>
2025-09-07 01:55:11 -0500

    Debugged the AI JS context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let topicInput = document.querySelector("#user-topic");
   let apiKey = "68a00b103e53ctc365f3574aeo9c2492";
   let context =
-    "You are an expert at writing nature poems for children. Your mission is to generate a 4 line in basic HTML and separate each line with a <br />. Do not include a title to the poem. Please sign the bottom of the poem 'SheCodes AI'. Make sure to follow user instructions.";
+    "You are an expert at writing nature poems for children. Your mission is to generate a 4 line in basic HTML format and separate each line with a <br />. Do not include a title to the poem. Please sign the bottom of the poem 'SheCodes AI'. Make sure to follow user instructions.";
   let prompt = `User instructions: Generate a poem about ${topicInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
